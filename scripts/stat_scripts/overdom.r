90aa6a90efc72fc4f0c4036875d47da5b4cd4387
BaronKoy <bkero1991@gmail.com>
2024-06-10 15:19:30 +0100

    stats&simulation script updates

--- scripts/stat_scripts/overdom.r
+++ scripts/stat_scripts/overdom.r
@@ -10,5 +10,10 @@ population <- read_csv('/home/baron/Documents/rotation_2/QM_rotation/scripts/out
 #View file if required
 #View(allele)
 
-cage_plot <- ggplot(population, aes(x = Generation, y = Minor_allele_frequency, color = Cage)) + geom_line()
+cage_plot <- ggplot(population, aes(x = Generation, y = Minor_allele_frequency, color = Cage)) + geom_line() +
+  theme_grey() +
+  labs(title = 'Alternate allele frequency per generation \nOverdominance (h=20.0, s=0.5)', y = 'Alternate allele frequency', x = 'Generations') +
+  scale_x_continuous(breaks = c(0,2,4,8,12,20,28,36,44,56)) +
+  scale_color_discrete(breaks = c(1,2,3,4,5,6,7,8,9,10))
+  
 cage_plot
